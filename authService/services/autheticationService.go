23c1076371a59a0e6478b11537da8fd13e7b438e
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-08-13 22:38:38 -0500

    Creación de servicio de login authenticationService.go

---
+++ authService/services/autheticationService.go
@@ -0,0 +1,29 @@
+package services
+
+import (
+	"errors"
+	"toDoListApp/authService/config"
+	"toDoListApp/authService/models"
+	"toDoListApp/authService/utils"
+)
+
+func UserAuthenticator(userName, password string) (string, models.Usuario, error) {
+	//Busqueda del usuario en la BD
+	var user models.Usuario
+	if err := config.DB.Where("user_name = ?", userName).First(&user).Error; err != nil {
+		return "", models.Usuario{}, errors.New("usuario incorrecto")
+	}
+
+	// Comparación de contraseñas usando Hash
+	if !utils.CheckPasswordHash(password, user.Password) {
+		return "", models.Usuario{}, errors.New("contraseña incorrecta")
+	}
+
+	//Generación de Token
+	token, err := utils.GenerateJWT(user.ID, user.UserName)
+	if err != nil {
+		return "", models.Usuario{}, errors.New("error durante la generacion del token")
+	}
+
+	return token, user, nil
+}
