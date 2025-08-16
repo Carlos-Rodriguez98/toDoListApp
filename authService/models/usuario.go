b5311ed8b95d22bad58e4092e2508685289f97a8
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-08-16 18:55:19 -0500

    Se modifica el atributo a Image por AvatarPath

--- authService/models/usuario.go
+++ authService/models/usuario.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Usuario struct {
-	ID        uint           `json:"id" gorm:"primaryKey;autoIncrement"`
-	UserName  string         `json:"userName" gorm:"unique;not null"`
-	Password  string         `json:"-" gorm:"not null"` //This information isn't returned.
-	Image     string         `json:"image"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdateAt  time.Time      `json:"updateAt"`
-	DeleteAt  gorm.DeletedAt `gorm:"index" json:"-"`
+	ID         uint           `json:"id" gorm:"primaryKey;autoIncrement"`
+	Username   string         `json:"username" gorm:"unique;not null"`
+	Password   string         `json:"-" gorm:"not null"` //This information isn't returned.
+	AvatarPath string         `json:"avatarPath"`
+	CreatedAt  time.Time      `json:"createdAt"`
+	UpdateAt   time.Time      `json:"updateAt"`
+	DeleteAt   gorm.DeletedAt `gorm:"index" json:"-"`
 }
