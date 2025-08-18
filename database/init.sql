63e114a05f4962d6024586ea737653ed4b7475ea
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-08-17 20:45:11 -0500

    Se elimina del insert el campo de id, para no generar inconsistencias en asignaciones posteriores

--- database/init.sql
+++ database/init.sql
@@ -10,21 +10,21 @@ CREATE TABLE IF NOT EXISTS usuarios(
 );
 
 -- Insertar datos en la tabla de usuarios
-INSERT INTO usuarios (id, username, password, avatar_path, created_at, updated_at, deleted_at) VALUES
-(1,'Benjamin', '$2a$12$abc123usuario01hash', '/static/default.jpg', '2025-07-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
-(2, 'CARLOS98', '$2a$12$abc123usuario02hash', '/static/default.jpg', '2025-07-19T05:01:22.410608857Z', '2025-07-19T05:01:22.410608857Z', NULL),
-(3, 'Carlos18', '$2a$12$abc123usuario03hash', '/static/default.jpg', '2025-07-23T06:06:22.410608857Z', '2025-07-23T06:06:22.410608857Z', NULL),
-(4, 'Andres', '$2a$12$abc123usuario04hash', '/static/default.jpg', '2025-07-27T08:43:22.410608857Z', '2025-07-27T08:43:22.410608857Z', NULL),
-(5, 'CamilaSuarez', '$2a$12$abc123usuario05hash', '/static/default.jpg', '2025-07-30T02:01:22.410608857Z', '2025-07-30T02:01:22.410608857Z', NULL),
-(6, 'Maicol Lopez', '$2a$12$abc123usuario06hash', '/static/default.jpg', '2025-08-01T05:01:22.410608857Z', '2025-08-01T05:01:22.410608857Z', NULL),
-(7, 'FranciscaE', '$2a$12$abc123usuario07hash', '/static/default.jpg', '2025-08-04T15:01:22.410608857Z', '2025-08-04T15:01:22.410608857Z', NULL),
-(8, 'Correo', '$2a$12$abc123usuario08hash', '/static/default.jpg', '2025-08-7T00:01:22.410608857Z', '2025-08-7T00:01:22.410608857Z', NULL),
-(9, '485787', '$2a$12$abc123usuario09hash', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
-(10, 'Gaston Fe', '$2a$12$abc123usuario10hash', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
-(11, 'Palmera', '$2a$12$abc123usuario11hash', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
-(12, 'Deadpool', '$2a$12$abc123usuario12hash', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
-(13, 'Correo PRUEBA', '$2a$12$abc123usuario13hash', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
-(14, 'Carlos98', '$2a$12$abc123usuario14hash', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL)
+INSERT INTO usuarios (username, password, avatar_path, created_at, updated_at, deleted_at) VALUES
+('Benjamin', '$2a$12$YW1ikzXms3OV8/wNfF4avOxi67/1MJfBHseqKCgbcusZTF.WztTbu', '/static/default.jpg', '2025-07-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
+('CARLOS98', '$2a$12$akd3aYgtsx2jrRpYZ9SvUeaU9isSc./tXVl36j8f7kxzw4Rc473VK', '/static/default.jpg', '2025-07-19T05:01:22.410608857Z', '2025-07-19T05:01:22.410608857Z', NULL),
+('Carlos18', '$2a$12$MqVjwxXzp1W2ZbyfW8EqyO94FYp10pddqVpdOJZrkrth98PANBm.u', '/static/default.jpg', '2025-07-23T06:06:22.410608857Z', '2025-07-23T06:06:22.410608857Z', NULL),
+('Andres', '$2a$12$DdFD7OlqqrBtAFk3kKOTV.Px9O9idbRq1VkRw5jXEB2lFds3LUfTC', '/static/default.jpg', '2025-07-27T08:43:22.410608857Z', '2025-07-27T08:43:22.410608857Z', NULL),
+('CamilaSuarez', '$2a$12$IUJIB1hR9s/j9Qh2x8oivePlMk5jCY.DAXTBry.Jo01X1/RqV1DTG', '/static/default.jpg', '2025-07-30T02:01:22.410608857Z', '2025-07-30T02:01:22.410608857Z', NULL),
+('Maicol Lopez', '$2a$12$tc5Zw/v3ES9sRk93..SZeeMZQawywb6JvnB2RNoONrMxzJgGtbwVO', '/static/default.jpg', '2025-08-01T05:01:22.410608857Z', '2025-08-01T05:01:22.410608857Z', NULL),
+('FranciscaE', '$2a$12$46yuHKn5uiZixS7nuh6gxutAcFG6iZQkq8MDcK9nbtSbOELm7BRcy', '/static/default.jpg', '2025-08-04T15:01:22.410608857Z', '2025-08-04T15:01:22.410608857Z', NULL),
+('Correo', '$2a$12$m7WrXGkuOsE9spvVOOYf7OCsnBdgg/deFr5dh7HTVSP6SrwMFONce', '/static/default.jpg', '2025-08-7T00:01:22.410608857Z', '2025-08-7T00:01:22.410608857Z', NULL),
+('485787', '$2a$12$5yt9NzJu3Y565LyYiemM5O8LQnXmVYExrLO.OcEW9rpWPDiX8f1L2', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
+('Gaston Fe', '$2a$12$ZjV0woMT/R/I8mTWdQ7v/uhEN/ZJCAKPGjniYs2feyqyPqvEGpDDi', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
+('Palmera', '$2a$12$AZ9pjThmv8a4NRE5ZGop7eT7MXZRfW2VrYQGRcfT.snDRb86AwNUS', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
+('Deadpool', '$2a$12$dwE8HXNZcmYmfQiCNQOxBuujUr2UdSEeQ9HCTNG4zs3p1JIAAKhR2', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
+('Correo PRUEBA', '$2a$12$29ajc1GO3G0m7MAOL0RSIO.XET.NXfXqGTdKuU4ZDnec2l9BUgepi', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL),
+('Carlos98', '$2a$12$sToX6YvezfEN0zaQEegKVOXi19f0V96F.kQBRmWXE7BRKGFOJ4LF2', '/static/default.jpg', '2025-08-17T05:01:22.410608857Z', '2025-08-17T05:01:22.410608857Z', NULL)
 ON CONFLICT DO NOTHING;
 
 -- Definición tabla de categoría
@@ -37,21 +37,21 @@ CREATE TABLE IF NOT EXISTS categorias(
 
 
 -- Insertar datos en la tabla de categorias
-INSERT INTO categorias (id, name, description, created_at) VALUES
-(1, 'Trabajos', 'Trabajos universidad', '2025-07-17 12:00:00'),
-(2, 'Trabajos', 'Trabajos universidad', '2025-07-19 12:00:00'),
-(3, 'Proyectos', 'Proyectos migración Apps Empresa', '2025-07-20 12:35:00'),
-(4, 'Hogar', '', '2025-07-17 12:01:22'),
-(5, 'Estudio', 'Actividades de estudio', '2025-07-24 05:02:48'),
-(6, 'Salud', 'Actividades para mejorar hábitos', '2025-07-28 01:48:12'),
-(7, 'Finanzas', 'Validaciones de mis finanzas', '2025-07-30 20:08:01'),
-(8, 'Social/Ocio', 'Actividades de descanso', '2025-08-08 08:08:08'),
-(9, 'Estudio', 'Actividades de estudio matemáticas', '2025-08-10 10:12:00'),
-(10, 'Finanzas', 'Validaciones de mis finanzas', '2025-08-13 17:53:25'),
-(11, 'Hogar', 'Actividades para el apartamento', '2025-07-17 12:00:00'),
-(12, 'Proyectos/creatividad', 'Creación de contenido', '2025-08-15 04:05:06'),
-(13, 'Estudio', '', '2025-07-17 12:00:00'),
-(14, 'Finanzas', '', '2025-08-17 17:17:17')
+INSERT INTO categorias (name, description, created_at) VALUES
+('Trabajos', 'Trabajos universidad', '2025-07-17 12:00:00'),
+('Trabajos', 'Trabajos universidad', '2025-07-19 12:00:00'),
+('Proyectos', 'Proyectos migración Apps Empresa', '2025-07-20 12:35:00'),
+('Hogar', '', '2025-07-17 12:01:22'),
+('Estudio', 'Actividades de estudio', '2025-07-24 05:02:48'),
+('Salud', 'Actividades para mejorar hábitos', '2025-07-28 01:48:12'),
+('Finanzas', 'Validaciones de mis finanzas', '2025-07-30 20:08:01'),
+('Social/Ocio', 'Actividades de descanso', '2025-08-08 08:08:08'),
+('Estudio', 'Actividades de estudio matemáticas', '2025-08-10 10:12:00'),
+('Finanzas', 'Validaciones de mis finanzas', '2025-08-13 17:53:25'),
+('Hogar', 'Actividades para el apartamento', '2025-07-17 12:00:00'),
+('Proyectos/creatividad', 'Creación de contenido', '2025-08-15 04:05:06'),
+('Estudio', '', '2025-07-17 12:00:00'),
+('Finanzas', '', '2025-08-17 17:17:17')
 ON CONFLICT DO NOTHING;
 
 -- Definición tabla de de tareas
